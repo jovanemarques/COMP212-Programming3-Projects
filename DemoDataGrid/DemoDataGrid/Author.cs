dd46eb6756a49a73ad3af6dbd8c39c64e37ef7f0
Jovane Marques <jovanemarques@gmail.com>
2019-01-30 01:50:27 -0500

    DemoDataGrid

---
+++ DemoDataGrid/DemoDataGrid/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DemoDataGrid
+{
+    class Author
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string BookTitle { get; set; }
+        public bool IsMVP { get; set; }
+    }
+}
---
+++ DemoDataGrid/DemoDataGrid/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace DemoDataGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Author> authors = new ObservableCollection<Author>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadCollectionData();
+            myDG.ItemsSource = authors;
+        }
+        private void LoadCollectionData()
+        {
+            authors.Add(new Author()
+            {
+                ID = 101,
+                Name = "Cindy Smith",
+                BookTitle = "C# Programming",
+                DOB = new DateTime(1975, 1, 1),
+                IsMVP = false
+            });
+
+            authors.Add(new Author()
+            {
+                ID = 101,
+                Name = "Tom Smith",
+                BookTitle = "Java Programming",
+                DOB = new DateTime(1966, 1, 1),
+                IsMVP = true
+            });
+        }
+    }
+}
