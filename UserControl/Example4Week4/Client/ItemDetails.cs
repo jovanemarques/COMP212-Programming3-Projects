8738dc762a34995a8004f31046c03e98b0931b94
Jovane Marques <jovanemarques@gmail.com>
2019-02-06 00:56:38 -0500

    UserControl Example

---
+++ UserControl/Example4Week4/Client/ItemDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class ItemDetails
+    {
+        public string Name { get; set; }
+        public int InitialValue { get; set; }
+
+        public ItemDetails(string name, int sValke)
+        {
+            Name = name;
+            InitialValue = sValke;
+        }
+    }
+}
---
+++ UserControl/Example4Week4/InventoryTrackerControl/InventoryTrackerControl.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InventoryTrackerControl
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class InventoryTrackerControl : UserControl
+    {
+        public InventoryTrackerControl()
+        {
+            InitializeComponent();
+        }
+
+
+        public string ProductItemName
+        {
+            get { return (string)GetValue(ProductItemNameProperty); }
+            set { SetValue(ProductItemNameProperty, value); }
+        }
+
+        public static DependencyProperty ProductItemNameProperty =
+           DependencyProperty.Register(nameof(ProductItemName), typeof(string), typeof(InventoryTrackerControl), new PropertyMetadata("Item Name"));
+
+        public int StartValue
+        {
+            get { return (int)GetValue(StartValueProperty); }
+            set { SetValue(StartValueProperty, value); }
+        }
+
+        public static DependencyProperty StartValueProperty =
+           DependencyProperty.Register(nameof(StartValue), typeof(int), typeof(InventoryTrackerControl),new PropertyMetadata(0));
+
+        public int CurrentValue
+        {
+            get { return (int)GetValue(CurrentValueProperty); }
+            set { SetValue(CurrentValueProperty, value); }
+        }
+
+        public static DependencyProperty CurrentValueProperty =
+           DependencyProperty.Register(nameof(CurrentValue), typeof(int), typeof(InventoryTrackerControl), new PropertyMetadata(0));
+    }
+}
