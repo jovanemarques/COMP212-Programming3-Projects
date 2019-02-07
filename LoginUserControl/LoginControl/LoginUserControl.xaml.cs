96253a3630a322c122dfaf48ab056dc89c0d9dfb
Jovane Marques <jovanemarques@gmail.com>
2019-02-07 01:32:22 -0500

    Assignment 2 - Exercise 2 - LoginUserControl

---
+++ LoginUserControl/LoginControl/LoginUserControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LoginControl
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class LoginUserControl : UserControl
+    {
+        public string Username
+        {
+            get { return (string)GetValue(UsernameProperty); }
+            set { SetValue(UsernameProperty, value); }
+        }
+        public static DependencyProperty UsernameProperty =
+            DependencyProperty.Register(nameof(Username), typeof(string), typeof(LoginUserControl),
+                new PropertyMetadata("Username"));
+        public string Password
+        {
+            get
+            {
+                return (string)GetValue(PasswordProperty);
+            }
+            set
+            {
+                SetValue(PasswordProperty, value);
+                txtPassword.Password = value;
+            }
+        }
+        public static DependencyProperty PasswordProperty =
+            DependencyProperty.Register(nameof(Password), typeof(string), typeof(LoginUserControl),
+                new PropertyMetadata("Password"));
+        public LoginUserControl()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
--- LoginUserControl/LoginUserControl/MainWindow.xaml.cs
+++ LoginUserControl/LoginUserControl/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace LoginUserControl
 {
@@ -23,6 +10,18 @@ namespace LoginUserControl
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (ucLogin.Username.Equals("jovane") && ucLogin.Password.Equals("123"))
+            {
+                MessageBox.Show("Login Sucessful");
+            }
+            else
+            {
+                MessageBox.Show("Login Fail");
+            }
         }
     }
 }
