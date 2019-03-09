cdff7a5adf0eb261091cc3c35e5fe5ce666e33a0
Jovane Marques <jovanemarques@gmail.com>
2019-03-09 15:05:15 -0500

    Examples

---
+++ BooksExamples/DisplayTable/DisplayAuthorsForm.cs
@@ -0,0 +1,34 @@
+﻿using BooksExamples;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisplayTable
+{
+    public partial class DisplayAuthorsForm : Form
+    {
+        private BooksEntities dbContext = new BooksEntities();
+        public DisplayAuthorsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayAuthorsForm_Load(object sender, EventArgs e)
+        {
+            dbContext.Authors.Load();
+
+            //(from author in dbContext.Authors             //use query syntax 
+            // orderby author.LastName, author.FirstName
+            // select author).Load();
+
+            authorBindingSource.DataSource = dbContext.Authors.Local;
+        }
+    }
+}
---
+++ CodeFirstNewDBSample/CodeFirstNewDBSample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstNewDBSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new BloggingContext();
+            Console.Write("Enter a name for a new Blog:");
+
+            var name = Console.ReadLine();
+
+            var blog = new Blog { Name = name };
+
+            db.Blogs.Add(blog);
+            db.SaveChanges();
+
+            var query = from b in db.Blogs
+                        orderby name
+                select b;
+
+            Console.WriteLine("All blogs in the database:");
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
