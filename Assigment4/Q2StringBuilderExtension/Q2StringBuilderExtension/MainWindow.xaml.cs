a7c7136d8d1efb80dc2b391e7900a820ee4515fd
Jovane Marques <jovanemarques@gmail.com>
2019-03-21 23:38:42 -0400

    Assignment 4

---
+++ Assigment4/Q2StringBuilderExtension/Q2StringBuilderExtension/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Q2StringBuilderExtension
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCount_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder str = new StringBuilder(txtString.Text);
+            //StringBuilder str = new StringBuilder("This is to test whether the extension method count can return a right answer or not");
+            lblResult.Content = "The count to the string is: " + str.CountWords();
+        }
+    }
+
+    public static class Extension
+    {
+        public static int CountWords(this StringBuilder strB)
+        {
+            String[] str = strB.ToString().Trim().Split(' ');
+            return str.Length;
+        }
+    }
+}
