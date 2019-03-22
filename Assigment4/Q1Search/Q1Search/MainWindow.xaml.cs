52c7b199690aaf2bea9019e307a26931ad4bb200
Jovane Marques <jovanemarques@gmail.com>
2019-03-22 19:58:27 -0400

    Assignment 4

--- Assigment4/Q1Search/Q1Search/MainWindow.xaml.cs
+++ Assigment4/Q1Search/Q1Search/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Q1Search
 {
@@ -20,9 +8,32 @@ namespace Q1Search
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int MAX_NUMS = 50;
+        const int MIN_INT = 1;
+        const int MAX_INT = 100;
+
+        Random r = new Random();
+        int[] ints = new int[MAX_NUMS];
+        double[] doubs = new double[MAX_NUMS];
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < MAX_NUMS; i++)
+            {
+                ints[i] = r.Next(MIN_INT, MAX_INT);
+                doubs[i] = (r.NextDouble() * 100);
+            }
+            lblInts.Content = "Int numbers are: ";
+            for (int i = 0; i < MAX_NUMS; i++)
+            {
+                lblInts.Content += ints[i] + ", ";
+                lblDouble.Content += doubs[i] + ", ";
+            }
         }
     }
 }
