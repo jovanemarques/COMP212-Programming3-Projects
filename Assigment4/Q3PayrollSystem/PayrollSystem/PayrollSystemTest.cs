759d422b38b47c804799c7d9a13ad21556b96ecc
Jovane Marques <jovanemarques@gmail.com>
2019-03-23 14:22:24 -0400

    Assignment 4 - Question 3 - Done

--- Assigment4/Q3PayrollSystem/PayrollSystem/PayrollSystemTest.cs
+++ Assigment4/Q3PayrollSystem/PayrollSystem/PayrollSystemTest.cs
@@ -2,67 +2,84 @@
 // Employee hierarchy test app.
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class PayrollSystemTest
 {
-   static void Main()
-   {
-      // create derived-class objects
-      var salariedEmployee = new SalariedEmployee("John", "Smith",
-          "111-11-1111", 800.00M);
-      var hourlyEmployee = new HourlyEmployee("Karen", "Price",
-         "222-22-2222", 16.75M, 40.0M);
-      var commissionEmployee = new CommissionEmployee("Sue", "Jones",
-         "333-33-3333", 10000.00M, .06M);
-      var basePlusCommissionEmployee =
-         new BasePlusCommissionEmployee("Bob", "Lewis",
-         "444-44-4444", 5000.00M, .04M, 300.00M);
+    static void Main()
+    {
+        // create derived-class objects
+        var salariedEmployee = new SalariedEmployee("John", "Smith",
+            "111-11-1111", 800.00M);
+        var hourlyEmployee = new HourlyEmployee("Karen", "Price",
+           "222-22-2222", 16.75M, 40.0M);
+        var commissionEmployee = new CommissionEmployee("Sue", "Jones",
+           "333-33-3333", 10000.00M, .06M);
+        var basePlusCommissionEmployee =
+           new BasePlusCommissionEmployee("Bob", "Lewis",
+           "444-44-4444", 5000.00M, .04M, 300.00M);
 
-      Console.WriteLine("Employees processed individually:\n");
+        Console.WriteLine("Employees processed individually:\n");
 
-      Console.WriteLine($"{salariedEmployee}\nearned: " +
-          $"{salariedEmployee.Earnings():C}\n");
-      Console.WriteLine(
-         $"{hourlyEmployee}\nearned: {hourlyEmployee.Earnings():C}\n");
-      Console.WriteLine($"{commissionEmployee}\nearned: " +
-          $"{commissionEmployee.Earnings():C}\n");
-      Console.WriteLine($"{basePlusCommissionEmployee}\nearned: " +
-          $"{basePlusCommissionEmployee.Earnings():C}\n");
-      
-      // create List<Employee> and initialize with employee objects
-      var employees = new List<Employee>() {salariedEmployee,
+        Console.WriteLine($"{salariedEmployee}\nearned: " +
+            $"{salariedEmployee.Earnings():C}\n");
+        Console.WriteLine(
+           $"{hourlyEmployee}\nearned: {hourlyEmployee.Earnings():C}\n");
+        Console.WriteLine($"{commissionEmployee}\nearned: " +
+            $"{commissionEmployee.Earnings():C}\n");
+        Console.WriteLine($"{basePlusCommissionEmployee}\nearned: " +
+            $"{basePlusCommissionEmployee.Earnings():C}\n");
+
+        // create List<Employee> and initialize with employee objects
+        var employees = new List<Employee>() {salariedEmployee,
          hourlyEmployee, commissionEmployee, basePlusCommissionEmployee};
 
-      Console.WriteLine("Employees processed polymorphically:\n");
+        Console.WriteLine("Employees processed polymorphically:\n");
 
-      // generically process each element in employees
-      foreach (var currentEmployee in employees)
-      {
-         Console.WriteLine(currentEmployee); // invokes ToString
+        // generically process each element in employees
+        foreach (var currentEmployee in employees)
+        {
+            Console.WriteLine(currentEmployee); // invokes ToString
 
-         // determine whether element is a BasePlusCommissionEmployee
-         if (currentEmployee is BasePlusCommissionEmployee)
-         {
-            // downcast Employee reference to 
-            // BasePlusCommissionEmployee reference
-            var employee = (BasePlusCommissionEmployee)currentEmployee;
+            // determine whether element is a BasePlusCommissionEmployee
+            if (currentEmployee is BasePlusCommissionEmployee)
+            {
+                // downcast Employee reference to 
+                // BasePlusCommissionEmployee reference
+                var employee = (BasePlusCommissionEmployee)currentEmployee;
 
-            employee.BaseSalary *= 1.10M;
-            Console.WriteLine("new base salary with 10% increase is: " +
-                $"{employee.BaseSalary:C}");
-         }
+                employee.BaseSalary *= 1.10M;
+                Console.WriteLine("new base salary with 10% increase is: " +
+                    $"{employee.BaseSalary:C}");
+            }
 
-         Console.WriteLine($"earned: {currentEmployee.Earnings():C}\n");
-            Console.ReadKey();
-      }
+            Console.WriteLine($"earned: {currentEmployee.Earnings():C}\n");
+        }
 
-      // get type name of each object in employees 
-      for (int j = 0; j < employees.Count; j++)
-      {
-         Console.WriteLine(
-            $"Employee {j} is a {employees[j].GetType()}");
-      }
-   }
+        // get type name of each object in employees 
+        for (int j = 0; j < employees.Count; j++)
+        {
+            Console.WriteLine(
+               $"Employee {j} is a {employees[j].GetType()}");
+        }
+
+        Console.WriteLine("-----------------Beginnig of Question 3 answer");
+        Console.WriteLine("-----------------by Jovane Marques - 300982100");
+        var employeesNewSalary = employees.Select(e =>
+        new
+        {
+            FullName = e.FirstName + " " + e.LastName,
+            Salary = e is BasePlusCommissionEmployee ? ((e as BasePlusCommissionEmployee).BaseSalary * Decimal.Parse("1.1")) : e.Earnings()
+        });
+
+        foreach (var emp in employeesNewSalary)
+        {
+            Console.WriteLine("Employee: " + emp.FullName + ", Salary = " + emp.Salary);
+        }
+        Console.WriteLine("-----------------End of Question 3 answer");
+
+        Console.ReadKey();
+    }
 }
 
 
