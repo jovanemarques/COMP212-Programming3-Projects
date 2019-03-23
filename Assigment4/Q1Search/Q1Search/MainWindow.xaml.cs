2e49d5943931bd34c43aa630e099b9a81ea6a62b
Jovane Marques <jovanemarques@gmail.com>
2019-03-23 12:00:49 -0400

    Assigment 4 - Question 1 - Done

--- Assigment4/Q1Search/Q1Search/MainWindow.xaml.cs
+++ Assigment4/Q1Search/Q1Search/MainWindow.xaml.cs
@@ -26,13 +26,62 @@ namespace Q1Search
             for (int i = 0; i < MAX_NUMS; i++)
             {
                 ints[i] = r.Next(MIN_INT, MAX_INT);
-                doubs[i] = (r.NextDouble() * 100);
+                doubs[i] = Math.Round((r.NextDouble() * 100), 2);
             }
-            lblInts.Content = "Int numbers are: ";
+            txtInts.Text = "The Int numbers are:\n";
+            txtDoubles.Text = "The Double numbers are:\n";
             for (int i = 0; i < MAX_NUMS; i++)
             {
-                lblInts.Content += ints[i] + ", ";
-                lblDouble.Content += doubs[i] + ", ";
+                txtInts.Text += ints[i] + ", ";
+                txtDoubles.Text += doubs[i] + ", ";
+            }
+        }
+        private int Search<T>(T item, T[] inputArray) where T : IComparable<T>
+        {
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i].CompareTo(item) == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNumber.Text.Trim() != "")
+            {
+                int valueInt;
+                double valueDoub;
+                if (Int32.TryParse(txtNumber.Text, out valueInt))
+                {
+                    int pos = Search<int>(valueInt, ints);
+                    if (pos > -1)
+                    {
+                        txtResult.Text = "The number is at position: " + (pos + 1);
+                    }
+                    else
+                    {
+                        txtResult.Text = "The number was not found.";
+                    }
+                }
+                else if (Double.TryParse(txtNumber.Text, out valueDoub))
+                {
+                    int pos = Search<double>(valueDoub, doubs);
+                    if (pos > -1)
+                    {
+                        txtResult.Text = "The number is at position: " + (pos + 1);
+                    }
+                    else
+                    {
+                        txtResult.Text = "The number was not found.";
+                    }
+                }
+                else
+                {
+                    txtResult.Text = "Invalid data.";
+                }
             }
         }
     }
